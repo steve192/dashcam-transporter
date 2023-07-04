079c724a45fa30b3a29326cf54c5acc2bf6d2e27
steve192 <stefan@sterul.com>
2023-07-04 19:53:27 +0200

    feat: check if platform is raspberry pi fixes #2

--- backend/src/RaspiLed.ts
+++ backend/src/RaspiLed.ts
@@ -8,9 +8,49 @@ export class RaspiLED {
   private static _operation: 'IDLE' | 'DASHCAMTRANSFER' | 'HOMETRANSFER' = 'IDLE'
   private static ledStatus = false
 
+  private static isPiCached: undefined | boolean = undefined
+
+  public static isRaspberryPi () {
+    if (this.isPiCached !== undefined) {
+      return this.isPiCached
+    }
+
+    const piModels = [
+      // https://www.raspberrypi.org/documentation/hardware/raspberrypi/
+      'BCM2708',
+      'BCM2709',
+      'BCM2710',
+      'BCM2835', // Raspberry Pi 1 and Zero
+      'BCM2836', // Raspberry Pi 2
+      'BCM2837', // Raspberry Pi 3 (and later Raspberry Pi 2)
+      'BCM2837B0', // Raspberry Pi 3B+ and 3A+
+      'BCM2711' // Raspberry Pi 4B
+    ]
+
+    let cpuInfo: string
+    try {
+      cpuInfo = fs.readFileSync('/proc/cpuinfo', { encoding: 'utf8' })
+    } catch (e) {
+      // No file found, expect no pi
+      return false
+    }
+
+    let isPi = false
+    piModels.forEach(model => {
+      if (cpuInfo.includes(model)) {
+        isPi = true
+      }
+    })
+
+    this.isPiCached = isPi
+    return this.isPiCached
+  }
+
   public static initialize () {
-    fs.writeFileSync('/sys/class/leds/led0/trigger', 'none')
-    fs.writeFileSync('/sys/class/leds/led1/trigger', 'none')
+    if (this.isRaspberryPi()) {
+      fs.writeFileSync('/sys/class/leds/led0/trigger', 'none')
+      fs.writeFileSync('/sys/class/leds/led1/trigger', 'none')
+    }
     console.log('LEDs setup')
     RaspiLED.updateStatus()
   }
@@ -22,8 +62,10 @@ export class RaspiLED {
       RaspiLED.ledStatus = true
     }
 
-    fs.writeFileSync('/sys/class/leds/led0/brightness', RaspiLED.ledStatus ? '1' : '0')
-    fs.writeFileSync('/sys/class/leds/led1/brightness', RaspiLED.ledStatus ? '1' : '0')
+    if (this.isRaspberryPi()) {
+      fs.writeFileSync('/sys/class/leds/led0/brightness', RaspiLED.ledStatus ? '1' : '0')
+      fs.writeFileSync('/sys/class/leds/led1/brightness', RaspiLED.ledStatus ? '1' : '0')
+    }
     setTimeout(RaspiLED.updateStatus, 500)
   }
 
