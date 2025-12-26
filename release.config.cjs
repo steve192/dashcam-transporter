0a3a5fdefd88c603b3431b778396c2bf48dd4800
steve192 <stefan@sterul.com>
2025-12-26 20:17:45 +0100

    chore: add chore commits to release notes

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   branches: ['main'],
   tagFormat: 'v${version}',
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      releaseRules: [
+        { type: 'chore', release: 'patch' }
+      ]
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github'
   ]
